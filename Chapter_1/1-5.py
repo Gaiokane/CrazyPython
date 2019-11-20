98b968898e2c927199d208d2b45eb146311c38ff
Gaiokane <qk632773809@vip.qq.com>
2019-11-20 21:37:30 +0800

    赋值/算术/索引运算符、math

---
+++ Chapter_1/1-5.py
@@ -0,0 +1,20 @@
+print(4.0/2.0)
+print(int(4.0/2.0))
+print(5%2)
+#余数的正负号总是与被除数有关
+print(23 % -7)# -7 * -4 - 23 = -5
+print(3 ** 3)#乘方
+print(4 ** 0.5)#开平方
+print(27 ** (1/3))#开立方
+
+import math
+print(math.sqrt(16))#math函数开平方
+
+a = 4
+a += 7
+print(a)
+
+#索引
+print('gaiokane'[7])
+print('gaiokane'[0:3])
+print('gaiokane'[0:7:3])
